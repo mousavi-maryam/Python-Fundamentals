40d0df809ef1ef6a8c7cb339ebd6b908a0547722
Maryam Mousavi <maryam.s.mousaavi@gmail.com>
2026-07-29 21:11:55 +0330

    Improve Chapter 3 Exercises

--- python-for-everybody/chapter-03/exercise-01-overtime_pay.py
+++ python-for-everybody/chapter-03/exercise-01-overtime_pay.py
@@ -1,14 +1,14 @@
 """
 Chapter 3-Exercise 1
 
-Objective
+Objective:
 Rewrite the pay computation program to calculate overtime pay. 
 Employees earn their regular hourly rate for the first 40 hours and 1.5 times the hourly rate for any additional hours worked.
 
-My Solution
+My Solution:
 Collected the number of working hours and the hourly pay rate from the user. 
 Used an `if`/`else` statement to calculate regular pay or overtime pay, 
-then displayed the final salary rounded to two decimal places.
+then displayed the final PAY rounded to two decimal places.
 """
 
 hours=float(input('please enter working hours:\n'))
